35d5c75114acbcdb0942fd74833ee45b5bb12887
Kacper Warda <kacper@compucorp.co.uk>
2018-01-31 17:57:53 +0100

    BASW-42: Moving ManualRecurringPayment class into CRM/Core namespace to be recognized as valid Payment class by CiviCRM.

---
+++ CRM/Core/Payment/ManualRecurringPayment.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class CRM_Core_Payment_ManualRecurringPayment.
+ *
+ * It's used by new Manual_Recurring_Payment payment processor type
+ * (created within CRM_Membership_Utils_PaymentProcessorType class)
+ * and for Offline_Recurring_Contribution payment processor
+ * (created within CRM_Membership_Utils_PaymentProcessor class).
+ */
+class CRM_Core_Payment_ManualRecurringPayment extends CRM_Core_Payment {
+
+  /**
+   * @inheritDoc
+   */
+  public function checkConfig() {
+  }
+}
