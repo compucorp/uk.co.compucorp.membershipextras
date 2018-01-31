d7cf98437bd02fe2207e6a596533d296eba67762
Kacper Warda <kacper@compucorp.co.uk>
2018-01-31 16:48:50 +0100

    BASW-42: Creating empty Payment class for Manual Recurring Payment payment type.

---
+++ CRM/Membership/Payment/ManualRecurringPayment.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class CRM_Membership_Payment_ManualRecurringPayment.
+ *
+ * It's used by new Manual_Recurring_Payment payment processor type
+ * (created within CRM_Membership_Utils_PaymentProcessorType class).
+ */
+class CRM_Membership_Payment_ManualRecurringPayment extends CRM_Core_Payment {
+
+  public function checkConfig() {
+  }
+}
