e79b6433c365c634904c587746680f48129150e4
Camilo Rodriguez <camilo@compucorp.co.uk>
2018-03-23 11:36:30 +0000

    BASW-67: Set Form as Class Attribute Set on Instantiation

--- CRM/MembershipExtras/Hook/BuildForm/Membership.php
+++ CRM/MembershipExtras/Hook/BuildForm/Membership.php
@@ -2,8 +2,11 @@
 
 class CRM_MembershipExtras_Hook_BuildForm_Membership {
   private $templatePath = '';
+  private $form;
 
-  public function __construct() {
+  public function __construct(CRM_Member_Form_Membership &$form) {
+    $this->form = $form;
+
     $path = realpath(dirname(__FILE__));
     $classPath = strtr(
       self::class,
@@ -12,22 +15,24 @@ class CRM_MembershipExtras_Hook_BuildForm_Membership {
     $this->templatePath = strtr($path, array($classPath => '')) . 'templates';
   }
 
-  public function buildForm(CRM_Member_Form_Membership &$form) {
-    $form->add('text', 'installments', ts('Number of Installments'), '', TRUE);
-    $form->addRule('installments', ts('Installments must be a number.'), 'numeric');
+  public function buildForm() {
+    if ($this->form->_action & CRM_Core_Action::ADD) {
+      $this->form->add('text', 'installments', ts('Number of Installments'), '', TRUE);
+      $this->form->addRule('installments', ts('Installments must be a number.'), 'numeric');
 
-    $form->add('text', 'installments_frequency', ts('Interval'), '', TRUE);
-    $form->addRule('installments_frequency', ts('Installments must be a number.'), 'numeric');
+      $this->form->add('text', 'installments_frequency', ts('Interval'), '', TRUE);
+      $this->form->addRule('installments_frequency', ts('Installments must be a number.'), 'numeric');
 
-    $form->add('select', 'installments_frequency_unit',
-      ts('Installments Frequency Units'),
-      array('' => ts('- select -')) + CRM_Core_OptionGroup::values('recur_frequency_units', FALSE, FALSE, TRUE),
-      TRUE
-    );
+      $this->form->add('select', 'installments_frequency_unit',
+        ts('Installments Frequency Units'),
+        array('' => ts('- select -')) + CRM_Core_OptionGroup::values('recur_frequency_units', FALSE, FALSE, TRUE),
+        TRUE
+      );
 
-    CRM_Core_Region::instance('page-body')->add(array(
-      'template' => "{$this->templatePath}/CRM/Member/Form/PaymentPlanToggler.tpl"
-    ));
+      CRM_Core_Region::instance('page-body')->add(array(
+        'template' => "{$this->templatePath}/CRM/Member/Form/PaymentPlanToggler.tpl"
+      ));
+    }
   }
 
 }
