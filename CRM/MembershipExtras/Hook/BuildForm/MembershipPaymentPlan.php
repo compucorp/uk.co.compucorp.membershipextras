5f38e7d4d3ac34a7af8a608b36244f38ea1f2d19
Omar abu hussein <opetmar91@gmail.com>
2018-05-03 04:01:32 +0300

    BASW-106: Add membership offline auto-renew support

--- CRM/MembershipExtras/Hook/BuildForm/MembershipPaymentPlan.php
+++ CRM/MembershipExtras/Hook/BuildForm/MembershipPaymentPlan.php
@@ -4,18 +4,18 @@
  * Implements form changes needed to be done to add payment plan as an option to
  * pay for a membership.
  */
-abstract class CRM_MembershipExtras_Hook_BuildForm_MembershipPaymentPlan {
+class CRM_MembershipExtras_Hook_BuildForm_MembershipPaymentPlan {
   /**
    * @var string
    *   Path where template with new fields is stored.
    */
-  protected $templatePath = '';
+  private $templatePath;
 
   /**
    * @var \CRM_Member_Form_Membership
    *   Form object that is being altered.
    */
-  protected $form;
+  private $form;
 
   /**
    * CRM_MembershipExtras_Hook_BuildForm_Membership constructor.
@@ -28,18 +28,6 @@ abstract class CRM_MembershipExtras_Hook_BuildForm_MembershipPaymentPlan {
   }
 
   /**
-   * This method should check if the given action is the proper operation so
-   * that the form is changed to have payment plan data (ie. new for membership
-   * form, renew for membership renewal form).
-   *
-   * @param int $action
-   *   Constant value for the action, as defined in CRM_Core_Action class
-   *
-   * @return boolean
-   */
-  protected abstract function isCorrectOperation($action);
-
-  /**
    * Implements changes to form loaded in $this->form
    */
   public function buildForm() {
@@ -50,11 +38,7 @@ abstract class CRM_MembershipExtras_Hook_BuildForm_MembershipPaymentPlan {
    * Adds new fields to the form and includes template that shows the fields and
    * juggles around exiting ones.
    */
-  protected function addPaymentPlanSection() {
-    if (!$this->isCorrectOperation($this->form->_action)) {
-      return;
-    }
-
+  private function addPaymentPlanSection() {
     $this->form->add('text', 'installments', ts('Number of Installments'), '', FALSE);
     $this->form->addRule('installments', ts('Installments must be a number.'), 'numeric');
 
