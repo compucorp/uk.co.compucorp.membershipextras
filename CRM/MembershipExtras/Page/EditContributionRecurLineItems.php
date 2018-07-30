3b4cc3bcf176cff2aef962988d3a717da6ad96d4
Camilo Rodriguez <camilo@compucorp.co.uk>
2018-07-30 11:10:40 +0000

    BASW-212: Implement Current Period Tab

--- CRM/MembershipExtras/Page/EditContributionRecurLineItems.php
+++ CRM/MembershipExtras/Page/EditContributionRecurLineItems.php
@@ -3,10 +3,63 @@ use CRM_MembershipExtras_ExtensionUtil as E;
 
 class CRM_MembershipExtras_Page_EditContributionRecurLineItems extends CRM_Core_Page {
 
+  /**
+   * ID of the recurring contribution being viewed.
+   *
+   * @var int
+   */
+  private $contribRecur;
+
+  /**
+   * List of available financial types in the system.
+   *
+   * @var array
+   */
+  private $financialTypes;
+
+  /**
+   * @inheritdoc
+   */
+  public function __construct($title = NULL, $mode = NULL) {
+    parent::__construct($title, $mode);
+
+    $this->contribRecur = $this->getRecurringContribution();
+    $this->financialTypes = $this->getFinancialTypes();
+  }
+
+  /**
+   * Loads recurring contribution identified by ID set in request.
+   *
+   * @return array
+   */
+  private function getRecurringContribution() {
+    $recurringContributionID = CRM_Utils_Request::retrieveValue('crid', 'Positive', 0);
+
+    return $result = civicrm_api3('ContributionRecur', 'getsingle', [
+      'id' => $recurringContributionID,
+    ]);
+  }
+
+  /**
+   * Loads available financial types.
+   *
+   * @return array
+   */
+  private function getFinancialTypes() {
+    $financialTypes = civicrm_api3('FinancialType', 'get', []);
+
+    return $financialTypes['values'];
+  }
+
+  /**
+   * @inheritdoc
+   */
   public function run() {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(E::ts('View/Update Recurring Line Items'));
 
+    $this->assign('periodStartDate', CRM_Utils_Array::value('start_date', $this->contribRecur));
+    $this->assign('periodEndDate', CRM_Utils_Array::value('end_date', $this->contribRecur));
     $this->assign('lineItems', $this->getLineItems());
 
     parent::run();
@@ -18,7 +71,44 @@ class CRM_MembershipExtras_Page_EditContributionRecurLineItems extends CRM_Core_
    * @return array
    */
   private function getLineItems() {
+    $lineItems = array();
 
+    $result = civicrm_api3('ContributionRecurLineItem', 'get', [
+      'sequential' => 1,
+      'contribution_recur_id' => $this->contribRecur['id'],
+      'api.LineItem.getsingle' => [
+        'id' => '$value.line_item_id',
+        'entity_table' => ['IS NOT NULL' => 1],
+        'entity_id' => ['IS NOT NULL' => 1]
+      ],
+    ]);
+
+    if ($result['count'] > 0) {
+      foreach ($result['values'] as $lineItemData) {
+        $lineDetails = $lineItemData['api.LineItem.getsingle'];
+        unset($lineItemData['api.LineItem.getsingle']);
+
+        $lineDetails['financial_type'] = $this->getFinancialTypeName($lineDetails['financial_type_id']);
+        $lineItems[] = array_merge($lineItemData, $lineDetails);
+      }
+    }
+
+    return $lineItems;
+  }
+
+  /**
+   * Returns the financil type's name identified by the given ID.
+   *
+   * @param int $id
+   *
+   * @return string
+   */
+  private function getFinancialTypeName($id) {
+    if (in_array($id, array_keys($this->financialTypes))) {
+      return $this->financialTypes[$id]['name'];
+    }
+
+    return '';
   }
 
 }
--- templates/CRM/MembershipExtras/Page/EditContributionRecurLineItems.tpl
+++ templates/CRM/MembershipExtras/Page/EditContributionRecurLineItems.tpl
@@ -1,3 +1,75 @@
-<h3></h3>
+<div id="periodsContainer" class="ui-tabs ui-widget ui-widget-content ui-corner-all">
+  {* Tab management *}
+  <script type="text/javascript">
+    var selectedTab  = 'contributions';
+    {literal}
+    CRM.$(function($) {
+      var tabIndex = $('#tab_' + selectedTab).prevAll().length;
+      $("#periodsContainer").tabs({active: tabIndex});
+      $(".crm-tab-button").addClass("ui-corner-bottom");
+    });
+    {/literal}
+  </script>
+  <ul class="ui-tabs-nav ui-corner-all ui-helper-reset ui-helper-clearfix ui-widget-header">
+    <li id="tab_current" class="crm-tab-button ui-corner-all ui-tabs-tab ui-corner-top ui-state-default ui-tab ui-tabs-active ui-state-active">
+      <a href="#current-subtab" title="{ts}Contributions{/ts}">
+        {ts}Current Period{/ts}
+      </a>
+    </li>
+    <li id="tab_next" class="crm-tab-button ui-corner-all ui-tabs-tab ui-corner-top ui-state-default ui-tab">
+      <a href="#next-subtab" title="{ts}Recurring Contributions{/ts}">
+        {ts}Next Period{/ts}
+      </a>
+    </li>
+  </ul>
 
-[ LINE TEM LIST ]
+  <div id="current-subtab" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
+    <div class="right">
+      Period Start Date: {$periodStartDate|date_format}
+      &nbsp;&nbsp;&nbsp;
+      Period End Date: {$periodEndDate|date_format}
+    </div>
+    <table class="selector row-highlight">
+      <tbody>
+      <tr class="columnheader">
+        <th scope="col">{ts}Item{/ts}</th>
+        <th scope="col">{ts}Start Date{/ts}</th>
+        <th scope="col">{ts}End Date{/ts}</th>
+        <th scope="col">{ts}Renew Automatically{/ts}</th>
+        <th scope="col">{ts}Financial Type{/ts}</th>
+        <th scope="col">{ts}Tax{/ts}</th>
+        <th scope="col">{ts}Amount{/ts}</th>
+        <th scope="col">&nbsp;</th>
+      </tr>
+      {foreach from=$lineItems item='currentItem'}
+        <tr id="lineitem-{$currentItem.id}" data-action="cancel" class="crm-entity {cycle values="odd-row,even-row"}">
+          <td>{$currentItem.label}</td>
+          <td>{$currentItem.start_date|date_format}</td>
+          <td>{$currentItem.end_date|date_format}</td>
+          <td>{$currentItem.auto_renew}</td>
+          <td>{$currentItem.financial_type}</td>
+          <td>{$currentItem.tax_amount|crmMoney}</td>
+          <td>{$currentItem.line_total|crmMoney}</td>
+          <td>
+            <a class="delete" href="">
+              <span><i class="crm-i fa-trash"></i></span>
+            </a>
+          </td>
+        </tr>
+      {/foreach}
+      </tbody>
+    </table>
+    <div id="current_buttons">
+      <a class="button" href="">
+        <span><i class="crm-i fa-plus"></i>&nbsp; Add Membership</span>
+      </a>
+      <a class="button" href="">
+        <span><i class="crm-i fa-plus"></i>&nbsp; Add Other Amount</span>
+      </a>
+    </div>
+  </div>
+  <div id="next-subtab" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
+    [ NEXT PERIOD ]
+  </div>
+  <div class="clear"></div>
+</div>
