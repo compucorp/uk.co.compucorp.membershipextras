70c075c7d52819b1f662c27531cc7b9f69ea179d
Camilo Rodriguez <camilo@compucorp.co.uk>
2018-08-17 01:02:26 +0000

    BASW-220: Add New Form to Confirm Membership Addition

---
+++ CRM/MembershipExtras/Form/RecurringContribution/AddMembershipLineItem.php
@@ -0,0 +1,32 @@
+<?php
+
+use CRM_MembershipExtras_ExtensionUtil as E;
+
+/**
+ * Form controller class to confirm addition of a new membership as a line item
+ * in a recurring contribution.
+ */
+class CRM_MembershipExtras_Form_RecurringContribution_AddMembershipLineItem extends CRM_Core_Form {
+
+  /**
+   * @inheritdoc
+   */
+  public function preProcess() {
+    
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function buildQuickForm() {
+
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function postProcess() {
+
+  }
+
+}
---
+++ templates/CRM/MembershipExtras/Form/RecurringContribution/AddMembershipLineItem.tpl
@@ -0,0 +1,14 @@
+<div class="crm-submit-buttons">
+{include file="CRM/common/formButtons.tpl" location="top"}
+</div>
+
+<p>
+  [ TEXT ]
+</p>
+<div class="crm-section">
+  [ INPUTS ]
+</div>
+
+<div class="crm-submit-buttons">
+{include file="CRM/common/formButtons.tpl" location="bottom"}
+</div>
