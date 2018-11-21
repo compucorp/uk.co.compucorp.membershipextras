3bb636a23bc05478b5b73b2b80248f83525a8a62
Camilo Rodriguez <camilo@compucorp.co.uk>
2018-11-21 14:15:28 +0000

    BASW-322: Define Required Prameters for Create Period API Call

--- api/v3/MembershipPeriod.php
+++ api/v3/MembershipPeriod.php
@@ -10,7 +10,10 @@ use CRM_MembershipExtras_ExtensionUtil as E;
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
  */
 function _civicrm_api3_membership_period_create_spec(&$spec) {
-  // $spec['some_parameter']['api.required'] = 1;
+  $spec['membership_id']['api.required'] = 1;
+  $spec['start_date']['api.required'] = 1;
+  $spec['payment_entity_table']['api.required'] = 1;
+  $spec['entity_id']['api.required'] = 1;
 }
 
 /**
