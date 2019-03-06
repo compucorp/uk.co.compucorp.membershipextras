d024904482c569ef11aaf62c11ab77636a511680
Kator James <kator95@gmail.com>
2019-03-06 16:42:01 +0100

    BASW-328: PR feedback

--- CRM/MembershipExtras/Job/OverdueMembershipPeriodProcessor.php
+++ CRM/MembershipExtras/Job/OverdueMembershipPeriodProcessor.php
@@ -14,14 +14,16 @@ class CRM_MembershipExtras_Job_OverdueMembershipPeriodProcessor {
    */
   public function run() {
     $membershipPeriodDao = $this->getMembershipsPeriodsWithOverduePayment();
-    
+    $errors = [];
     while ($membershipPeriodDao->fetch()) {
       try {
         $this->disableMembershipPeriod($membershipPeriodDao->membership_period_id);
       } catch (Exception $e) {
-        $message = "An error occurred disabling an overdue membership period with id({$membershipPeriodDao->membership_period_id}): " . $e->getMessage();
+        $errors[] = "An error occurred disabling an overdue membership period with id({$membershipPeriodDao->membership_period_id}): " . $e->getMessage();
+      }
 
-        throw new Exception($message);
+      if (count($errors) > 0) {
+        throw new Exception("Errors found while processing periods: " . implode('; ', $errors));
       }
     }
 
@@ -37,19 +39,37 @@ class CRM_MembershipExtras_Job_OverdueMembershipPeriodProcessor {
     $completedContributionStatusID = $contributionStatusesNameMap['Completed'];
     
     $daysToDisableMP = CRM_MembershipExtras_SettingsManager::getDaysToDisableMembershipPeriodsWithOverduePayment();
-    $dateNow = (new \DateTime())->format('Y-m-d H:i:s');
-    $daysAgo = date('Y-m-d H:i:s', strtotime("-$daysToDisableMP days", strtotime($dateNow)));
+    $date = new DateTime();
+    $date->sub(new DateInterval("P{$daysToDisableMP}D"));
+    $maxReceiveDate = $date->format('Y-m-d H:i:s');
 
     $query = "
+    (
       SELECT mmp.id as membership_period_id
         FROM membershipextras_membership_period mmp
-      LEFT JOIN civicrm_membership cm ON mmp.membership_id = cm.id
-      LEFT JOIN civicrm_membership_payment cmp ON mmp.membership_id = cmp.membership_id
-      LEFT JOIN civicrm_contribution cc ON cmp.contribution_id = cc.id
-        WHERE cc.receive_date <= '$daysAgo'
-         AND cc.contribution_status_id != $completedContributionStatusID
-         AND mmp.is_active = 1
-      GROUP BY membership_period_id
+          INNER JOIN civicrm_membership cm ON mmp.membership_id = cm.id
+          INNER JOIN civicrm_contribution cc ON (
+            mmp.entity_id = cc.id
+            AND mmp.payment_entity_table = 'civicrm_contribution'
+          )
+        WHERE cc.receive_date <= '{$maxReceiveDate}'
+          AND cc.contribution_status_id != {$completedContributionStatusID}
+          AND mmp.is_active = 1
+        GROUP BY membership_period_id
+    ) UNION (
+      SELECT mmp.id as membership_period_id
+        FROM membershipextras_membership_period mmp
+          INNER JOIN civicrm_membership cm ON mmp.membership_id = cm.id
+          INNER JOIN civicrm_contribution_recur ccr ON (
+            mmp.entity_id = ccr.id
+            AND mmp.payment_entity_table = 'civicrm_contribution_recur'
+          )
+          INNER JOIN civicrm_contribution cc ON ccr.id = cc.contribution_recur_id
+        WHERE cc.receive_date <= '{$maxReceiveDate}'
+          AND ccr.contribution_status_id != {$completedContributionStatusID}
+          AND mmp.is_active = 1
+        GROUP BY membership_period_id
+    )
     ";
 
     return CRM_Core_DAO::executeQuery($query);
