16af6db436e3b20296181f23cd802f6bbee7df12
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-07-02 13:25:37 +0100

    RSE-189: Add the membership dates class that allows using Civi's internal function to calculate membership dates. Refactor Tax calculation logic

--- CRM/MembershipExtras/Service/MembershipTypeTaxAmount.php
+++ CRM/MembershipExtras/Service/MembershipTypeTaxAmount.php
@@ -8,10 +8,12 @@ class CRM_MembershipExtras_Service_MembershipTypeTaxAmount {
    * Calculate Tax amount for the membership type.
    *
    * @param \CRM_Member_BAO_MembershipType $membershipType
+   * @param float $membershipTypeAmount
    *
    * @return mixed
    */
-  public function calculateTax(MembershipType $membershipType) {
+  public function calculateTax(MembershipType $membershipType, $membershipTypeAmount = NULL) {
+    $membershipTypeAmount = $membershipTypeAmount ? $membershipTypeAmount : $membershipType->minimum_fee;
     $taxRates = CRM_Core_PseudoConstant::getTaxRates();
     $taxAmount = 0;
     $taxRate = CRM_Utils_Array::value($membershipType->financial_type_id, $taxRates, 0);
@@ -20,6 +22,6 @@ class CRM_MembershipExtras_Service_MembershipTypeTaxAmount {
       return $taxAmount;
     }
 
-    return ($taxRate/100) * $membershipType->minimum_fee;
+    return ($taxRate/100) * $membershipTypeAmount;
   }
 }
