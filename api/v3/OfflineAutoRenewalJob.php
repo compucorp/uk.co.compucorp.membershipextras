5eaa79587f9f53f3f9eb3fcd1af9345e120bf7dd
Erawat Chamanont <erawat@gmail.com>
2020-07-23 15:51:35 +0100

    MAE-331: Refactor varaible as variable should start with lowercase

--- api/v3/OfflineAutoRenewalJob.php
+++ api/v3/OfflineAutoRenewalJob.php
@@ -7,10 +7,10 @@
  * @return array
  */
 function civicrm_api3_offline_auto_renewal_job_run($params) {
-  $OfflineAutoRenewalJob = new CRM_MembershipExtras_Job_OfflineAutoRenewal();
+  $offlineAutoRenewalJob = new CRM_MembershipExtras_Job_OfflineAutoRenewal();
 
   return civicrm_api3_create_success(
-    $OfflineAutoRenewalJob->run(),
+    $offlineAutoRenewalJob->run(),
     $params
   );
 }
