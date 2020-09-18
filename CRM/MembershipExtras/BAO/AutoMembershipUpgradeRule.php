0486a2a9224a3ceccad55f59a20f0c95c577029e
Omar abu hussein <opetmar91@gmail.com>
2020-09-18 04:02:59 +0300

    IO-72: Create membership renewal upgrade rule create/update form

--- CRM/MembershipExtras/BAO/AutoMembershipUpgradeRule.php
+++ CRM/MembershipExtras/BAO/AutoMembershipUpgradeRule.php
@@ -2,24 +2,53 @@
 
 class CRM_MembershipExtras_BAO_AutoMembershipUpgradeRule extends CRM_MembershipExtras_DAO_AutoMembershipUpgradeRule {
 
-  /**
-   * Create a new AutoMembershipUpgradeRule based on array-data
-   *
-   * @param array $params key-value pairs
-   * @return CRM_MembershipExtras_DAO_AutoMembershipUpgradeRule|NULL
-   *
   public static function create($params) {
     $className = 'CRM_MembershipExtras_DAO_AutoMembershipUpgradeRule';
     $entityName = 'AutoMembershipUpgradeRule';
+
     $hook = empty($params['id']) ? 'create' : 'edit';
-
     CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
-    $instance = new $className();
-    $instance->copyValues($params);
-    $instance->save();
-    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
 
-    return $instance;
-  } */
+    $params['name'] = strtolower(str_replace(' ', '_', $params['label']));
+    if ($hook == 'create') {
+      $params['weight'] = self::calculateUpgradeRuleWeight();
+    }
+
+    $newUpgradeRule = new $className();
+    $newUpgradeRule->copyValues($params);
+    $newUpgradeRule->save();
+
+    CRM_Utils_Hook::post($hook, $entityName, $newUpgradeRule->id, $newUpgradeRule);
+
+    return $newUpgradeRule;
+  }
+
+  /**
+   * Calculates the new upgrade
+   * rule weight based on the maximum
+   * existing weight.
+   *
+   * @return int
+   */
+  private static function calculateUpgradeRuleWeight() {
+    $upgradeRule = new self();
+    $upgradeRule->orderBy('weight DESC');
+    $upgradeRule->limit(1);
+    $upgradeRule->find(TRUE);
+
+    if (!empty($upgradeRule->weight)) {
+     return $upgradeRule->weight + 1;
+    }
+
+    return 1;
+  }
+
+  public static function getById($id) {
+    $upgradeRule = new self();
+    $upgradeRule->id = $id;
+    $upgradeRule->find(TRUE);
+
+    return $upgradeRule;
+  }
 
 }
