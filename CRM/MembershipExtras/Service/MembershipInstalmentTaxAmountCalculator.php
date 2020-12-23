6cf3dfd2bec399f75adfa13bb0c10f23a6d0db8c
Erawat Chamanont <erawat@gmail.com>
2020-12-23 15:04:14 +0000

    IO-122: Refactor tax amount calculator to support price field value

--- CRM/MembershipExtras/Service/MembershipInstalmentTaxAmountCalculator.php
+++ CRM/MembershipExtras/Service/MembershipInstalmentTaxAmountCalculator.php
@@ -2,27 +2,65 @@
 
 use CRM_Member_BAO_MembershipType as MembershipType;
 
-class CRM_MembershipExtras_Service_MembershipTypeTaxAmountCalculator {
+/**
+ * Class CRM_MembershipExtras_Service_MembershipInstalmentTaxAmountCalculator
+ */
+class CRM_MembershipExtras_Service_MembershipInstalmentTaxAmountCalculator {
 
   /**
-   * Calculate Tax amount for the membership type.
+   * @var array
+   */
+  private $taxRates;
+
+  /**
+   * CRM_MembershipExtras_Service_MembershipInstalmentTaxAmountCalculator constructor.
+   */
+  public function __construct() {
+    $this->taxRates = CRM_Core_PseudoConstant::getTaxRates();
+  }
+
+  /**
+   * Calculates Tax amount for the membership type.
    *
    * @param \CRM_Member_BAO_MembershipType $membershipType
    * @param float $membershipTypeAmount
    *
    * @return mixed
    */
-  public function calculateTax(MembershipType $membershipType, $membershipTypeAmount = NULL) {
+  public function calculateByMembershipType(MembershipType $membershipType, $membershipTypeAmount = NULL) {
     $membershipTypeAmount = $membershipTypeAmount ? $membershipTypeAmount : $membershipType->minimum_fee;
-    $taxRates = CRM_Core_PseudoConstant::getTaxRates();
-    $taxAmount = 0;
-    $taxRate = CRM_Utils_Array::value($membershipType->financial_type_id, $taxRates, 0);
+    $taxRate = CRM_Utils_Array::value($membershipType->financial_type_id, $this->taxRates, 0);
 
+    return $this->calculateTaxAmount($membershipTypeAmount, $taxRate);
+
+  }
+
+  /**
+   * Calculates Tax amount for the price field value
+   *
+   * @param array $priceFieldValue
+   * @return float|int
+   */
+  public function calculateByPriceFieldValue(array $priceFieldValue) {
+    $taxRate = CRM_Utils_Array::value($priceFieldValue['financial_type_id'], $this->taxRates, 0);
+
+    return $this->calculateTaxAmount($priceFieldValue['amount'], $taxRate);
+  }
+
+  /**
+   * Calculate tax amount from amount and tax rate
+   *
+   * @param $amount
+   * @param $taxRate
+   * @return float|int
+   */
+  private function calculateTaxAmount($amount, $taxRate) {
+    $taxAmount = 0;
     if (empty($taxRate)) {
       return $taxAmount;
     }
 
-    return ($taxRate/100) * $membershipTypeAmount;
+    return ($taxRate / 100) * $amount;
   }
 
 }
