2f09591522122e1e70bfe238715a9c11fed47c6b
Camilo Rodriguez <camilo@compucorp.co.uk>
2021-01-07 02:23:41 +0000

    MAE-447: Calculate Start Date From Previous End Date on Manual Renewal

--- CRM/MembershipExtras/Service/MembershipEndDateCalculator.php
+++ CRM/MembershipExtras/Service/MembershipEndDateCalculator.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Class CRM_MembershipExtras_Service_MembershipEndDateCalculator.
+ */
 class CRM_MembershipExtras_Service_MembershipEndDateCalculator {
 
   /**
@@ -9,15 +12,68 @@ class CRM_MembershipExtras_Service_MembershipEndDateCalculator {
    * @param int $membershipId
    *
    * @return string
+   * @throws \CiviCRM_API3_Exception
+   * @throws \CiviCRM_API3_Exception|\Exception
    */
   public static function calculate($membershipId) {
-    $membershipDetails = civicrm_api3('Membership', 'get', [
+    $newEndDate = 'null';
+    $membershipDetails = self::getMembership($membershipId);
+    $interval = self::getMembershipPeriodInterval($membershipDetails);
+    if (!empty($interval)) {
+      $currentEndDate = new DateTime($membershipDetails['end_date']);
+      $currentEndDate->add(new DateInterval($interval));
+      $newEndDate = $currentEndDate->format('Ymd');
+    }
+
+    return $newEndDate;
+  }
+
+  /**
+   * Calculates the previous end date for the given membership.
+   *
+   * @param int $membershipID
+   *
+   * @return string
+   * @throws \CiviCRM_API3_Exception|\Exception
+   */
+  public static function calculatePreviousEndDate($membershipID) {
+    $previousEndDate = 'null';
+    $membershipDetails = self::getMembership($membershipID);
+    $interval = self::getMembershipPeriodInterval($membershipDetails);
+
+    if (!empty($interval)) {
+      $currentEndDate = new DateTime($membershipDetails['end_date']);
+      $currentEndDate->sub(new DateInterval($interval));
+      $previousEndDate = $currentEndDate->format('Ymd');
+    }
+
+    return $previousEndDate;
+  }
+
+  /**
+   * Obtains information of the membership with the given ID.
+   *
+   * @param int $membershipID
+   *
+   * @return mixed
+   * @throws \CiviCRM_API3_Exception
+   */
+  private static function getMembership($membershipID) {
+    return civicrm_api3('Membership', 'get', [
       'sequential' => 1,
       'return' => ['end_date', 'membership_type_id.duration_unit', 'membership_type_id.duration_interval'],
-      'id' => $membershipId,
+      'id' => $membershipID,
     ])['values'][0];
+  }
 
-    $currentEndDate = new DateTime($membershipDetails['end_date']);
+  /**
+   * Calculates the period string to be used to calculate membership end date.
+   *
+   * @param array $membershipDetails
+   *
+   * @return string|null
+   */
+  private static function getMembershipPeriodInterval(array $membershipDetails) {
     switch ($membershipDetails['membership_type_id.duration_unit']) {
       case 'month':
         $interval = 'P' . $membershipDetails['membership_type_id.duration_interval'] . 'M';
@@ -36,13 +92,7 @@ class CRM_MembershipExtras_Service_MembershipEndDateCalculator {
         break;
     }
 
-    $newEndDate = 'null';
-    if (!empty($interval)) {
-      $currentEndDate->add(new DateInterval($interval));
-      $newEndDate = $currentEndDate->format('Ymd');
-    }
-
-    return $newEndDate;
+    return $interval;
   }
 
 }
