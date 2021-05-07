0e33b22d2623e3d21230a12229b1584bfbb5e3d5
Erawat Chamanont <erawat@gmail.com>
2021-05-07 10:04:38 +0100

    CIVIPLUS-176: Add administer Membership Extras permission

--- membershipextras.php
+++ membershipextras.php
@@ -134,8 +134,8 @@ function membershipextras_civicrm_navigationMenu(&$menu) {
     'name' => 'payment_plan_settings',
     'label' => ts('Payment Plan Settings'),
     'url' => 'civicrm/admin/payment_plan_settings',
-    'permission' => 'administer CiviCRM',
-    'operator' => NULL,
+    'permission' => 'administer CiviCRM,administer MembershipExtras',
+    'operator' => 'OR',
     'separator' => NULL,
   ];
 
@@ -145,8 +145,8 @@ function membershipextras_civicrm_navigationMenu(&$menu) {
     'name' => 'automated_membership_upgrade_rules',
     'label' => ts('Membership Automated Upgrade Rules'),
     'url' => 'civicrm/admin/member/automated-upgrade-rules?reset=1',
-    'permission' => 'administer CiviCRM',
-    'operator' => NULL,
+    'permission' => 'administer CiviCRM,administer MembershipExtras',
+    'operator' => 'OR',
     'separator' => 2,
   ];
   _membershipextras_civix_insert_navigation_menu($menu, 'Administer/CiviMember', $automatedMembershipUpgradeRulesMenuItem);
@@ -466,4 +466,17 @@ function membershipextras_civicrm_preProcess($formName, $form) {
 function membershipextras_civicrm_alterMailParams(&$params, $context) {
   $alterMailParamsHook = new CRM_MembershipExtras_Hook_Alter_MailParamsHandler($params);
   $alterMailParamsHook->handle();
+}
+
+/**
+ * Implements hook_civicrm_permission().
+ *
+ */
+function membershipextras_civicrm_permission(&$permissions) {
+  $permissions += [
+    'administer MembershipExtras' => [
+      E::ts('MembershipExtras: administer Membership Extras'),
+      E::ts('Perform all Membership Extras administration tasks in CiviCRM'),
+    ],
+  ];
 }
