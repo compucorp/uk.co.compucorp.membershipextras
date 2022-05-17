fb6e28c8651b834e0f5239df58f30cfdcc53e9e9
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2022-05-17 07:20:01 +0100

    MAE-690: Change same day month calculation

--- CRM/MembershipExtras/Service/InstalmentReceiveDateCalculator.php
+++ CRM/MembershipExtras/Service/InstalmentReceiveDateCalculator.php
@@ -88,7 +88,7 @@ class CRM_MembershipExtras_Service_InstalmentReceiveDateCalculator {
    * class will render the date in a way that
    * we do not desire, for example adding one month
    * to 2018-01-31 will result in 2018-03-03 but
-   * we want it to be 2018-02-28 instead.
+   * we want it to be 2018-03-01 instead.
    *
    * This method ensure that adding months
    * to a date get calculated properly.
@@ -100,30 +100,13 @@ class CRM_MembershipExtras_Service_InstalmentReceiveDateCalculator {
    * @return DateTime
    */
   public function getSameDayNextMonth(DateTime $startDate, $numberOfMonthsToAdd = 1) {
-    $startDateDay = (int) $startDate->format('j');
-    $startDateMonth = (int) $startDate->format('n');
-    $startDateYear = (int) $startDate->format('Y');
-
-    $numberOfYearsToAdd = floor(($startDateMonth + $numberOfMonthsToAdd) / 12);
-    if ((($startDateMonth + $numberOfMonthsToAdd) % 12) === 0) {
-      $numberOfYearsToAdd--;
+    $intervalSpec = "P{$numberOfMonthsToAdd}M";
+    if (in_array($startDate->format('j'), [29, 30, 31])) {
+      $startDate->modify('first day of next month');
     }
-    $year = $startDateYear + $numberOfYearsToAdd;
+    $startDate->add(new DateInterval($intervalSpec));
 
-    $month = ($startDateMonth + $numberOfMonthsToAdd) % 12;
-    if ($month === 0) {
-      $month = 12;
-    }
-    $month = sprintf('%02s', $month);
-
-    $numberOfDaysInMonth = (new DateTime("$year-$month-01"))->format('t');
-    $day = $startDateDay;
-    if ($startDateDay > $numberOfDaysInMonth) {
-      $day = $numberOfDaysInMonth;
-    }
-    $day = sprintf('%02s', $day);
-
-    return new DateTime("$year-$month-$day");
+    return $startDate;
   }
 
 }
