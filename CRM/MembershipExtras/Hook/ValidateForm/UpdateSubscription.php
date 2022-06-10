40f8971a072bf13d2dbbac7e9995058e7da9363f
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2022-06-10 19:16:58 +0100

    MAE-739: Validate next contrib date doesn't exceed 28 when updating the recurring contribution
    Ensure that the next schedule contribution for recurring contribution date can not be updated to date with a day above 28 for monthly contribution and to Feb 29 for an annual contribution

---
+++ CRM/MembershipExtras/Hook/ValidateForm/UpdateSubscription.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Class CRM_MembershipExtras_Hook_ValidateForm_UpdateSubscription
+ *
+ * Validates Membership Type Form
+ */
+class CRM_MembershipExtras_Hook_ValidateForm_UpdateSubscription {
+
+  /**
+   * Form object that is being validated.
+   *
+   * @var \CRM_Member_Form_Membership
+   */
+  private $form;
+
+  /**
+   * @var array
+   *   List of the submitted fields and their values passed from the hook.
+   */
+  private $fields;
+
+  /**
+   * @var array
+   *   List of form validation errors passed from the hook.
+   */
+  private $errors;
+
+  /**
+   * Array with the data of the recurring contribution that is being updated.
+   *
+   * @var array
+   */
+  private $recurringContribution;
+
+  const INVALID_NEXT_CONTRIB_DATE_MONTH = 'Not all months have more than 28 days. As such monthly or quarterly payment plans must renew on or before the 28th of the month. Please select another date on or before the 28th of the month.';
+  const INVALID_NEXT_CONTRIB_DATE_YEAR = 'Not all years have a 29th of Feb. As such annual payment plans must renew on any other day of the year!';
+
+  /**
+   * CRM_MembershipExtras_Hook_ValidateForm_UpdateSubscription constructor.
+   *
+   * @param \CRM_Contribute_Form_UpdateSubscription $form
+   * @param $fields
+   * @param $errors
+   */
+  public function __construct($form, &$fields, &$errors) {
+    $this->form = $form;
+    $this->fields = &$fields;
+    $this->errors = &$errors;
+    $this->setRecurringContribution();
+  }
+
+  /**
+   * Validates the subscription form submission
+   */
+  public function validate() {
+    $this->validateNextContributionDate();
+  }
+
+  private function validateNextContributionDate() {
+    $frequency = $this->recurringContribution['frequency_unit'];
+    $nextContribDate = $this->fields['next_sched_contribution_date'];
+    $nextContribDay = date('j', strtotime($nextContribDate));
+    $nextContibMonth = date('n', strtotime($nextContribDate));
+    if ($nextContribDay > 28 && $frequency === 'month') {
+      $this->errors['next_sched_contribution_date'] = ts(self::INVALID_NEXT_CONTRIB_DATE_MONTH);
+    }
+
+    if ($nextContribDay > 28 && $nextContibMonth == '2' && $frequency === 'year') {
+      $this->errors['next_sched_contribution_date'] = ts(self::INVALID_NEXT_CONTRIB_DATE_YEAR);
+    }
+  }
+
+  /**
+   * Loads data for recurring contribution identified by 'crid' parameter in
+   * http request.
+   */
+  private function setRecurringContribution() {
+    $recurringContributionID = CRM_Utils_Request::retrieve('crid', 'Integer', $this->form, TRUE);
+    $this->recurringContribution = civicrm_api3('ContributionRecur', 'get', [
+      'sequential' => 1,
+      'id' => $recurringContributionID,
+    ])['values'][0];
+  }
+
+}
