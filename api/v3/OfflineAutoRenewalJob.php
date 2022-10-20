cdb1503d0a0f9d667c1e9539464d7038d8213aae
Omar Hussein <opetmar91@gmail.com>
2022-10-20 11:01:45 +0100

    CIVIIB-11: Allow only one instance of auto-renewal job to run

--- api/v3/OfflineAutoRenewalJob.php
+++ api/v3/OfflineAutoRenewalJob.php
@@ -7,10 +7,23 @@
  * @return array
  */
 function civicrm_api3_offline_auto_renewal_job_run($params) {
-  $offlineAutoRenewalJob = new CRM_MembershipExtras_Job_OfflineAutoRenewal();
+  $lock = Civi::lockManager()->acquire('worker.membershipextras.offlineautorenewal');
+  if (!$lock->isAcquired()) {
+    return civicrm_api3_create_error('Could not acquire lock, another Offline Autorenewal process is running');
+  }
+
+  try {
+    $offlineAutoRenewalJob = new CRM_MembershipExtras_Job_OfflineAutoRenewal();
+    $result = $offlineAutoRenewalJob->run();
+    $lock->release();
+  }
+  catch (Exception $error) {
+    $lock->release();
+    throw $error;
+  }
 
   return civicrm_api3_create_success(
-    $offlineAutoRenewalJob->run(),
+    $result,
     $params
   );
 }
