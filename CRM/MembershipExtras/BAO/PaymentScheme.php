d5a95ccb425be524267560c1306394d98f35954c
Erawat Chamanont <erawat@gmail.com>
2023-02-08 16:50:19 +0000

    CIWEMB-135: Add create payment scheme

--- CRM/MembershipExtras/BAO/PaymentScheme.php
+++ CRM/MembershipExtras/BAO/PaymentScheme.php
@@ -3,4 +3,25 @@ use CRM_MembershipExtras_ExtensionUtil as E;
 
 class CRM_MembershipExtras_BAO_PaymentScheme extends CRM_MembershipExtras_DAO_PaymentScheme {
 
+  /**
+   * Create a new PaymentScheme based on array-data
+   *
+   * @param array $params key-value pairs
+   *
+   * @return CRM_MembershipExtras_BAO_PaymentScheme|NULL
+   */
+  public static function create($params) {
+    $className = 'CRM_MembershipExtras_BAO_PaymentScheme';
+    $entityName = 'PaymentScheme';
+    $hook = empty($params['id']) ? 'create' : 'edit';
+
+    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
+    $instance = new $className();
+    $instance->copyValues($params);
+    $instance->save();
+    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
+
+    return $instance;
+  }
+
 }
---
+++ CRM/MembershipExtras/Form/PaymentScheme.php
@@ -0,0 +1,98 @@
+<?php
+
+use CRM_MembershipExtras_ExtensionUtil as E;
+
+/**
+ * Form controller class
+ *
+ * @see https://docs.civicrm.org/dev/en/latest/framework/quickform/
+ */
+class CRM_MembershipExtras_Form_PaymentScheme extends CRM_Core_Form {
+
+  public function preProcess() {
+    if ($this->_action == CRM_Core_Action::UPDATE) {
+      CRM_Utils_System::setTitle(E::ts('Edit Payment Scheme'));
+    }
+    else {
+      CRM_Utils_System::setTitle(E::ts('Add Payment Scheme'));
+    }
+
+    parent::preProcess();
+  }
+
+  public function buildQuickForm() {
+    $this->add('text', 'name', E::ts('Name'), NULL, TRUE);
+    $this->add('text', 'admin_title', E::ts('Admin Title'), NULL, TRUE);
+    $this->add('textarea', 'admin_description', E::ts('Admin Description'));
+    $this->add('text', 'public_title', E::ts('Public Title'), NULL, TRUE);
+    $this->add('textarea', 'public_description', E::ts('Public Description'));
+    $this->add(
+      'select',
+      'permission',
+      'permission',
+      [
+        'public' => E::ts('Public'),
+        'admin' => E::ts('Admin'),
+      ],
+      TRUE
+    );
+    $this->add('checkbox', 'enabled', E::ts('Enabled'), NULL, FALSE);
+    $this->add('textarea', 'parameters', E::ts('Parameters'), NULL, TRUE);
+
+    $this->addButtons([
+      [
+        'type' => 'submit',
+        'name' => ts('Submit'),
+        'isDefault' => TRUE,
+      ],
+      [
+        'type' => 'cancel',
+        'name' => ts('Cancel'),
+      ],
+    ]);
+
+    $this->assign('elementNames', $this->getRenderableElementNames());
+    parent::buildQuickForm();
+  }
+
+  public function postProcess() {
+    $values = $this->exportValues();
+
+    if (!empty($this->id)) {
+      $params['id'] = $this->id;
+    }
+
+    $params = [
+      'name' => $values['name'],
+      'admin_title' => $values['admin_title'],
+      'admin_description' => $values['admin_description'],
+      'public_title' => $values['public_title'],
+      'public_description' => $values['public_description'],
+      'permission' => $values['permission'],
+      'enabled' => CRM_Utils_Array::value('enabled', $values, FALSE),
+      'parameters' => $values['parameters'],
+    ];
+
+    CRM_MembershipExtras_BAO_PaymentScheme::create($params);
+    CRM_Core_Session::setStatus(ts('The payment scheme has been saved.'), ts('Saved'), 'success');
+
+    parent::postProcess();
+  }
+
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array (string)
+   */
+  public function getRenderableElementNames() {
+    $elementNames = [];
+    foreach ($this->_elements as $element) {
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
+  }
+
+}
