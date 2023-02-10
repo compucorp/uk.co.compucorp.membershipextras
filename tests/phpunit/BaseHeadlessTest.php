2c6b7454b0b6e5476c521871a9e05311f507733f
Erawat Chamanont <erawat@gmail.com>
2023-02-10 15:55:04 +0000

    CIWEMB-100: Fix linter on existing test class

--- tests/phpunit/BaseHeadlessTest.php
+++ tests/phpunit/BaseHeadlessTest.php
@@ -4,11 +4,13 @@ use Civi\Test\HeadlessInterface;
 use Civi\Test\TransactionalInterface;
 
 abstract class BaseHeadlessTest extends PHPUnit_Framework_TestCase implements
-  HeadlessInterface, TransactionalInterface {
+    HeadlessInterface,
+    TransactionalInterface {
 
   public function setUpHeadless() {
     return \Civi\Test::headless()
       ->installMe(__DIR__)
       ->apply();
   }
+
 }
