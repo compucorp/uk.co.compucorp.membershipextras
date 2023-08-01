f29ad39d1f73275a05d1b8cb2b2c22d1d0da0db9
Omar Hussein <opetmar91@gmail.com>
2023-08-01 13:26:04 +0100

    CIWEMB-433: Fix wrong Membership pre edit tests

--- tests/phpunit/CRM/MembershipExtras/Hook/Pre/MembershipEditTest.php
+++ tests/phpunit/CRM/MembershipExtras/Hook/Pre/MembershipEditTest.php
@@ -118,7 +118,7 @@ class CRM_MembershipExtras_Hook_Pre_MembershipEditTest extends BaseHeadlessTest 
     $hook = new MembershipEditHook($membershipParams['id'], $membershipParams, $contributions[0]['id'], $paymentType);
     $hook->preProcess();
 
-    $this->assertTrue(!isset($membership['end_date']));
+    $this->assertFalse(array_key_exists('end_date', $membershipParams));
   }
 
   public function testPreventExtendingPaymentPlanMembershipOnRecordingPayment() {
@@ -138,13 +138,13 @@ class CRM_MembershipExtras_Hook_Pre_MembershipEditTest extends BaseHeadlessTest 
     $paymentType = 'owed';
 
     $tmpGlobals = [];
-    $tmpGlobals['_REQUEST']['entryURL'] = 'action=add&id=' . $contributions[0]['id'];
+    $tmpGlobals['_REQUEST']['entryURL'] = '?action=add&id=' . $contributions[0]['id'];
     CRM_Utils_GlobalStack::singleton()->push($tmpGlobals);
 
     $hook = new MembershipEditHook($membershipParams['id'], $membershipParams, NULL, $paymentType);
     $hook->preProcess();
 
-    $this->assertTrue(!isset($membership['end_date']));
+    $this->assertFalse(array_key_exists('end_date', $membershipParams));
     CRM_Utils_GlobalStack::singleton()->pop();
   }
 
@@ -164,7 +164,7 @@ class CRM_MembershipExtras_Hook_Pre_MembershipEditTest extends BaseHeadlessTest 
     $paymentType = 'owed';
 
     $tmpGlobals = [];
-    $tmpGlobals['_REQUEST']['q'] = 'civicrm/contribute/search';
+    $tmpGlobals['_GET']['q'] = 'civicrm/contribute/search';
     $tmpGlobals['_REQUEST']['_qf_Status_next'] = 'Update Pending Status';
     $tmpGlobals['_REQUEST']['contribution_status_id'] = '1';
     CRM_Utils_GlobalStack::singleton()->push($tmpGlobals);
@@ -172,7 +172,7 @@ class CRM_MembershipExtras_Hook_Pre_MembershipEditTest extends BaseHeadlessTest 
     $hook = new MembershipEditHook($membershipParams['id'], $membershipParams, NULL, $paymentType);
     $hook->preProcess();
 
-    $this->assertTrue(!isset($membership['end_date']));
+    $this->assertFalse(array_key_exists('end_date', $membershipParams));
     CRM_Utils_GlobalStack::singleton()->pop();
   }
 
