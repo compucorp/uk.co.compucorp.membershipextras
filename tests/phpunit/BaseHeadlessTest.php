ada66ba812a9692cf0f81b6ac2f8519aa97f7728
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-11-16 06:30:41 +0100

    BTHAB-284: Upgrade to Civix 22.05.2

--- tests/phpunit/BaseHeadlessTest.php
+++ tests/phpunit/BaseHeadlessTest.php
@@ -3,12 +3,14 @@
 use Civi\Test\HeadlessInterface;
 use Civi\Test\TransactionalInterface;
 
-abstract class BaseHeadlessTest extends PHPUnit_Framework_TestCase implements
-  HeadlessInterface, TransactionalInterface {
+abstract class BaseHeadlessTest extends PHPUnit\Framework\TestCase implements
+    HeadlessInterface,
+    TransactionalInterface {
 
   public function setUpHeadless() {
     return \Civi\Test::headless()
       ->installMe(__DIR__)
       ->apply();
   }
+
 }
