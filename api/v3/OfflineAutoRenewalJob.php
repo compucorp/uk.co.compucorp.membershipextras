5afb53ea51d68b4eaddb37c96efa8e00eca155dd
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2025-09-11 15:53:30 +0100

    CIWEMB-619: Separate offline autorenewal jobs
    Auto renewal jobs are now separated into single and multiple instalments job for individual processing

--- api/v3/OfflineAutoRenewalJob.php
+++ api/v3/OfflineAutoRenewalJob.php
@@ -1,20 +1,48 @@
 <?php
 
 /**
- * Membership offline auto-renewal scheduled job API
+ * Membership offline single instalment auto-renewal scheduled job API
  *
  * @param $params
  * @return array
  */
-function civicrm_api3_offline_auto_renewal_job_run($params) {
-  $lock = Civi::lockManager()->acquire('worker.membershipextras.offlineautorenewal');
+function civicrm_api3_offline_auto_renewal_job_runsingle($params) {
+  $lock = Civi::lockManager()->acquire('worker.membershipextras.offlinemultipleautorenewal');
   if (!$lock->isAcquired()) {
     return civicrm_api3_create_error('Could not acquire lock, another Offline Autorenewal process is running');
   }
 
   try {
     $offlineAutoRenewalJob = new CRM_MembershipExtras_Job_OfflineAutoRenewal();
-    $result = $offlineAutoRenewalJob->run();
+    $result = $offlineAutoRenewalJob->runSingle();
+    $lock->release();
+  }
+  catch (Exception $error) {
+    $lock->release();
+    throw $error;
+  }
+
+  return civicrm_api3_create_success(
+    $result,
+    $params
+  );
+}
+
+/**
+ * Membership offline multiple instalments auto-renewal scheduled job API
+ *
+ * @param $params
+ * @return array
+ */
+function civicrm_api3_offline_auto_renewal_job_runmultiple($params) {
+  $lock = Civi::lockManager()->acquire('worker.membershipextras.offlinesingleautorenewal');
+  if (!$lock->isAcquired()) {
+    return civicrm_api3_create_error('Could not acquire lock, another Offline Autorenewal process is running');
+  }
+
+  try {
+    $offlineAutoRenewalJob = new CRM_MembershipExtras_Job_OfflineAutoRenewal();
+    $result = $offlineAutoRenewalJob->runMultiple();
     $lock->release();
   }
   catch (Exception $error) {
